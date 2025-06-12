ebe714f8265993ec735923cdb14760a37108da54
Daniel Trubač <40642559+dant02@users.noreply.github.com>
2025-06-12 13:35:25 +0200

    lm studio tutorial

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello world');
+// tutorial using lm studio https://lmstudio.ai/docs/typescript
+
+import { LMStudioClient } from "@lmstudio/sdk";
+
+async function main(): Promise<void> {
+    const client = new LMStudioClient();
+
+    const model = await client.llm.model("deepseek/deepseek-r1-0528-qwen3-8b");
+    const result = await model.respond("What is the meaning of life?");
+
+    console.info(result.content);
+}
+
+main().catch(console.error);
