f5bd4164d3baefb05fd575a00ce661646dae353d
Daniel Trubač <40642559+dant02@users.noreply.github.com>
2025-06-13 09:47:36 +0200

    trial adding file contents to chat

--- app/app.ts
+++ app/app.ts
@@ -1,13 +1,34 @@
 // tutorial using lm studio https://lmstudio.ai/docs/typescript
 
-import { LMStudioClient } from "@lmstudio/sdk";
+import { Chat, LMStudioClient } from "@lmstudio/sdk";
+import fs from 'fs';
 
 async function main(): Promise<void> {
     const client = new LMStudioClient();
 
-    const model = await client.llm.model("deepseek/deepseek-r1-0528-qwen3-8b");
-    const result = await model.respond("What is the meaning of life?");
+    const model = await client.llm.model('gemma-3-27b-it-qat', {
+        config: {
+            contextLength: 8192,
+        },
+    });
 
+    console.info("Context length: " + await model.getContextLength());
+
+    const root = 'D:\\elektlabs\\libnet\\ElektLabs.DataCore\\ElektLabs.DataCore\\';
+
+    const dbFactory = await fs.promises.readFile(root + 'DBFactory.cs', 'utf8');
+    const dbContext = await fs.promises.readFile(root + 'Connections\\DBContext.cs', 'utf8');
+
+    const chat = Chat.from([
+        //{ role: "user", content: 'Remember following code block for later queries: ```csharp\n' + dbFactory + '\n' },
+        { role: "user", content: 'Remember following code block for later queries: ```csharp\n' + dbContext + '\n' },
+        { role: "user", content: "Your senior .net developer, perform code review of class DBContext in previous code blocks." },
+    ]);
+
+    const formatted = await model.applyPromptTemplate(chat);
+    const result = await model.complete(formatted, {
+        maxTokens: 8192,
+    });
     console.info(result.content);
 }
 
